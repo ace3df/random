c650594cc954660e75ca0dddac200bbfb0f66f1a
James <ace3df@gmail.com>
2014-02-28 18:02:28 +0000

    Update BatchOsuDownload.py

--- BatchOsuDownload.py
+++ BatchOsuDownload.py
@@ -1,6 +1,11 @@
-import sys, os, urllib2
+import sys, os, urllib2, time, requests
 from bs4 import BeautifulSoup
 from twill.commands import *
+
+# Uses BS4 and Twill
+# BS4: http://www.crummy.com/software/BeautifulSoup/
+# Twill: http://twill.idyll.org/
+# or you can download the .exe here: (twill is dumb and wont compile with py2exe)
 
 # Set agent so we can connect to Osu! site
 agent("Mozilla/5.0 (Windows NT 6.2; WOW64; rv:15.0) Gecko/20120910144328 Firefox/15.0.2")
@@ -16,6 +21,27 @@ fv("4", "password", password)
 # Submit!
 submit()
 
+def download_map(url1, bmapname):
+	
+	# Replace the 2 things that can stop windows from writing to file
+	bmapname = bmapname.replace("*", "(star)")
+	bmapname = bmapname.replace(":", "")
+
+	# Give new agent
+	agent("Opera/9.25 (Windows NT 5.1; U; en)")
+	# Go to generated url
+	dl = go(url1)
+	# Request to download
+	r = requests.get(dl, stream=True)
+
+	# Save in chunks
+	with open(bmapname, 'wb') as f:
+	    for chunk in r.iter_content(chunk_size=10024): 
+	        if chunk: # filter out keep-alive new chunks
+	            f.write(chunk)
+	            f.flush()
+	return
+
 # Osu! link batch list (url on each line)
 fname = "osubatch.txt"
 
@@ -23,15 +49,23 @@ fname = "osubatch.txt"
 with open(fname, 'r') as f:
     lines = f.readlines()
 
+# If you download too many you will get stopped so we just add a delay so it wont happen
+x = 0
+
 # For every line in file do something
 for url in lines:
+
+	x = x + 1
+	if x == 10:
+		time.sleep(100)
+	else:
+		time.sleep(20)
 
 	# First we'll get beatmap title so we can set it as the filename
 	request = urllib2.Request(url)
 	response = urllib2.urlopen(request)
 	soup = BeautifulSoup(response)
-
-	bmapname = soup.find('div', class_='content-with-bg').h1.text.encode('utf-8')[19:]
+	bmapname = soup.find('div', class_='content-with-bg').h1.text.encode('utf-8')[19:] + ".osz"
 
 	# Go to the beatmap URL
 	go(url)
@@ -45,7 +79,9 @@ for url in lines:
 		# Loop all links untill we find the link with /d/ in it
 		for line in link_list:
 			line = str(line)
-			if "/d/" in line:
+			if "google" in line: # google docs link has /d/ in it lol
+				pass
+			elif "/d/" in line:
 				temp = line
 
 		# Clean up the url line to get just the end of the url
@@ -55,8 +91,10 @@ for url in lines:
 		temp = temp[0]
 		# Site + temp url = download /d/ link!
 		osulink = "http://osu.ppy.sh" + str(temp)
+		print osulink
 		# Go to /d/ link
-		go(osulink)
+	download_map(osulink, bmapname)
 
 	# save_html is also just download so we download the map and save it as the map title + filetype
-	save_html(bmapname + ".osz")
+	print bmapname + " - Downloaded!"
+#	download_map(downloadlink, bmapname)

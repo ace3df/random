bd8d3eb80fe4a9a88ec8ddd1aa726a12c1986a13
James <ace3df@gmail.com>
2015-03-14 18:27:00 +0000

    Get ID by image fixes all.

--- BeatmapMirror.js
+++ BeatmapMirror.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name       osu! Beatmap Mirror
-// @version    1.4
+// @version    1.5
 // @description  Adds a mirror link to osu! download pages
 // @include     http*://osu.ppy.sh/b/*
 // @include     http*://osu.ppy.sh/s/*
-// @include     http*://osu.ppy.sh/p/beatmaplist
+// @include     http*://osu.ppy.sh/p/beatmaplist*
 // @copyright   Ace3DF, 2014
 // @updateURL   https://raw.githubusercontent.com/ace3df/random/master/BeatmapMirror.js
 // @grant       none
@@ -25,17 +25,15 @@ for(i = 0; i < search.length; i++){
 }
 
 // Beatmap Page
-// Get Beatmap ID from URL and split up - /s/ /beatmapID/
-var osuURL = window.location.pathname.split( '/' );
-// Only request the ID
-var beatmapID = osuURL[2];
-// Clean URL to make sure we get beatmap ID
-var beatmapID = beatmapID.split('&')[0];
+var beatmapID = document.getElementsByClassName('bmt')[0].src
+beatmapID = beatmapID.replace(/^.*[\\\/]/, '');
+beatmapID = beatmapID.replace(/l.jpg/, '');
+
 // Check if URL is a number (map) and not just a normal page
 if (!isNaN(beatmapID)) { 
-// Find the header HTML to edit and add the links
-	var html = document.getElementsByTagName("h1")[0]; // Get location to add the link to the site
-	html.innerHTML = html.innerHTML + "<br>Mirror Links »<a href='http://bloodcat.com/osu/m/" + beatmapID + "'> Bloodcat</a>"; //Add Bloodcat
-	html.innerHTML = html.innerHTML + " - <a href='http://loli.al/s/" + beatmapID + "'> loli</a>"; //Add loli
-	html.innerHTML = html.innerHTML + " - <a href='http://osu.uu.gl/s/" + beatmapID + "'> uu.gl</a>"; //Add uu.gl
+    // Find the header HTML to edit and add the links
+    var html = document.getElementsByTagName("h1")[0]; // Get location to add the link to the site
+    html.innerHTML = html.innerHTML + "<br>Mirror Links »<a href='http://bloodcat.com/osu/m/" + beatmapID + "'> Bloodcat</a>"; //Add Bloodcat
+    html.innerHTML = html.innerHTML + " - <a href='http://loli.al/s/" + beatmapID + "'> loli</a>"; //Add loli
+    html.innerHTML = html.innerHTML + " - <a href='http://osu.uu.gl/s/" + beatmapID + "'> uu.gl</a>"; //Add uu.gl
 }
